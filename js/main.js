dc2ce1e7bbdee7a118f0119d9310543b5dff6b8c
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-22 03:35:16 -0300

    Feature: função de status do projeto.

--- js/main.js
+++ js/main.js
@@ -17,3 +17,37 @@ let closeMenu = function(){
 hamIcon.addEventListener('click', openMenu);
 close.addEventListener('click', closeMenu);
 
+function tagStatus(){
+    let statusTag = document.querySelectorAll('.status');
+    for(let i = 0; i < statusTag.length; i++){
+        let status = '';
+        status = statusTag[i].textContent;
+        switch (status) {
+            case 'concluído':
+                    statusTag[i].style.backgroundColor = '#2f963b';
+                    statusTag[i].style.color = '#ebebeb';
+                break;
+            case 'em progresso':
+                    statusTag[i].style.backgroundColor = '#edac2a';
+                    statusTag[i].style.color = '#404040';
+                break;
+            case 'iniciado':
+                    statusTag[i].style.backgroundColor = '#5fb2bb';
+                    statusTag[i].style.color = '#404040';
+                break;
+            default:
+                    statusTag[i].textContent = 'Ops! Status inválido...';
+                    statusTag[i].style.backgroundColor = '#e74c4c';
+                    statusTag[i].style.color = '#ebebeb';
+                break;
+        }
+    }
+    
+}
+
+function main() {  
+    tagStatus();
+}
+
+
+window.load = main();
