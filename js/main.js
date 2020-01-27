d76b79acb8b2578ea306b47810bb52bd36f1f259
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-27 11:54:50 -0300

    Fix: toggle problem.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-/** Preciso resolver isso */
 function togglePrice(){
     let monthly = document.querySelectorAll('.monthly-price');
     let annually = document.querySelectorAll('.annually-price');
-
-    for(let i = 0; i < monthly.length; i++){
-        if(monthly[i].style.display == 'none'){
-            monthly[i].style.display = 'block';
-            annually[i].style.display = 'none'
-        } else{
+    let toogleBtn = document.querySelector('#toggle-price');
+    
+    if(toogleBtn.checked){
+        for(let i = 0; i < monthly.length; i++){
+            annually[i].style.display = 'none';
+            monthly[i].style.display = 'inline-table';
+            monthly[i].style.width = '100%';
+        }   
+    } else{
+        for(let i = 0; i < annually.length; i++){
             monthly[i].style.display = 'none';
-            annually[i].style.display = 'block';
+            annually[i].style.display = 'inline-table';
+            annually[i].style.width = '100%';
         }
     }
 }
