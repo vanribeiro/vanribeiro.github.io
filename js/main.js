8647051abb77b39c0d5c299c501291d87de0fc04
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-31 16:13:18 -0300

    Test: validation function. 11/11 Passed.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-exports.emailValidation = (email) => {
-    let re = /\w+/;
-    let answer = re.test(email);
-    return answer;
+const input = document.querySelector('#email');
+const msg = document.querySelector('#msg');
+
+/**
+ * This function was written using the RegExp of below link:
+ * https://codigofonte.com.br/codigos/validacao-completa-de-email-com-javascript-e-expressao-regular
+ */
+function isAnEmail (email) {
+    let exclude = /[^@\.\w]|^[\_@\.\-]|[\.\_\-]{2}|[@\.]{2}|(@)[^@]*\1/;
+    let check = /@[\w\-]+./;
+    let checkend = /\.[a-zA-Z]{2,3}$/;
+    if(((email.search(exclude) != -1)||
+        (email.search(check) == -1))||
+        (email.search(checkend) == -1)){
+            return false;
+    } else{
+        return true;
+    }
 }
 
-// //input.addEventListener('submit', validationEmail());
-// //let input = document.querySelector('#email-address');
+let validationEmail = () =>{
+    let p = document.createElement('p');
+    if(isAnEmail(input.value) == false){
+        p.appendChild = 'Please, provide a valid e-mail!'
+    }
+    msg.appendChild(p);
+}
+
+input.addEventListener('submit', validationEmail());
+
+
+// exports.isAnEmail = isAnEmail;
