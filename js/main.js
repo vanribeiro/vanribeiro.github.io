744375d62cf65b325c07e3385e0978f362c0f709
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-02-02 02:22:03 -0300

    Clear: commented code.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const input = document.querySelector('#email');
 const msg = document.querySelector('#msg');
 const form = document.querySelector('form');
+
 /**
  * This function was written using the RegExp of below link:
  * https://codigofonte.com.br/codigos/validacao-completa-de-email-com-javascript-e-expressao-regular
  */
+
 function isAnEmail (email) {
     let exclude = /[^@\.\w]|^[\_@\.\-]|[\.\_\-]{2}|[@\.]{2}|(@)[^@]*\1/;
     let check = /@[\w\-]+./;
@@ -29,7 +31,3 @@ form.addEventListener('submit', (event) =>{
     }
     msg.appendChild(p);
 });
-
-
-
-// exports.isAnEmail = isAnEmail;
