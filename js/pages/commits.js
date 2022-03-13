ab93990fd2a6baa97e3dab72b6f60dfc3f05f626
vanribeiro <vanribeiro.dev@gmail.com>
2022-03-13 18:44:59 -0300

    add commits page style

--- js/pages/commits.js
+++ js/pages/commits.js
@@ -1,5 +1,20 @@
 import getGithubRepoData from "../service/api-github.js";
+import getAluraDashboardData from "../service/api-alura.js";
 
 const githubCardsContainer = document.querySelector('.github-repos__cards');
+const aluraCardContainer = document.querySelector('.course-in-progress__all-items');
 
 getGithubRepoData(githubCardsContainer);
+getAluraDashboardData(aluraCardContainer);
+
+setTimeout(() => {
+
+    const allProgressBar = document.querySelectorAll('.progress-bar');
+    allProgressBar.forEach(progressBar => {
+        const pertcentual = progressBar.dataset.barValue;
+        progressBar.style.width = `${pertcentual}%`;
+        console.log(progressBar.dataset.barValue)
+    })
+
+}, 500);
+
--- js/templates/index.js
+++ js/templates/index.js
@@ -1,9 +1,11 @@
-const gitCardRepoTemplate = (repoName, repoDescription, repoLastUpdate, repoProgrammingLanguage, repoURL) =>{
+const githubCardRepoTemplate = (repoName, repoDescription, repoLastUpdate, repoProgrammingLanguage, repoURL) =>{
+
     const date = new Date(repoLastUpdate);
     const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
     const mounth = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
     const year = date.getFullYear();
-    const repoLastDayUpdate = `${day}/${mounth}/${year}`
+    const repoLastDayUpdate = `${day}/${mounth}/${year}`;
+
     return `
         <div class="github-repo__card__container card__container">
             <header class="github-repo__header">
@@ -30,8 +32,30 @@ const gitCardRepoTemplate = (repoName, repoDescription, repoLastUpdate, repoProg
             </div>
         </div>
     `;
+
+}
+
+const aluraStudyngTemplate = (courseName, courseProgress, courseSlug) =>{
+
+    return `
+        <header class="course-in-progress__item-header">
+            <a href="https://cursos.alura.com.br/course/${courseSlug}" title="Informações sobre o curso" target="blank" rel="noopener">
+                <h4 class="courses-in-progress__item-title">
+                    ${courseName}
+                </h4>
+            </a>
+        </header>
+        <div class="progress-bar__container">
+            <div class="progress-bar__background">
+                <div class="progress-bar" data-bar-value="${courseProgress}"></div>
+            </div>
+            <span class="progress-bar__percentual">${courseProgress}%</span>
+        </div>
+    `;
+
 }
 
 export{
-    gitCardRepoTemplate
+    githubCardRepoTemplate,
+    aluraStudyngTemplate
 }
