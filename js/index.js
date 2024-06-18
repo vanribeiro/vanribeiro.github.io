1d1057ada4b8a936795da26b8cf9d4be8a2c504f
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-06-18 00:08:18 -0300

    ocultar menu e páginas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-import menu from "./components/menu.js";
-import openMenuMobile from './handlers/menu-mobile.js';
+// import menu from "./components/menu.js";
+// import openMenuMobile from './handlers/menu-mobile.js';
 
-window.addEventListener('load', menu());
+// window.addEventListener('load', menu());
 
-const openButtonMenuMobile = document.querySelector('.humburger__icon');
+// const openButtonMenuMobile = document.querySelector('.humburger__icon');
 
-openButtonMenuMobile.addEventListener('click', openMenuMobile);
+// openButtonMenuMobile.addEventListener('click', openMenuMobile);
